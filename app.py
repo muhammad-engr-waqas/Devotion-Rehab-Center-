3f6455164b1728cafe73ca61c021ab9b3a5c2695
Engr_WaQaS-JUTT <aapka-vercel-account-email@example.com>
2026-09-07 19:38:23 +0500

    update

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ app.config["MONGO_URI"] = mongo_uri
 
 secret_key = os.environ.get("SECRET_KEY", "06e4b4738ab81f94277a7216b5e79fb24b339f28a6a131391d8d6f8f0a295dc1")
 app.config["SECRET_KEY"] = secret_key
-app.config["GMAIL_USER"] = os.environ.get("GMAIL_USER")
+app.config["GMAIL_USER"] = os.environ.get("GMAIL_USER", "hosiptalms@gmail.com")
 app.config["GMAIL_APP_PASSWORD"] = os.environ.get("GMAIL_APP_PASSWORD")
 app.config["PASSWORD_RESET_EXPIRY_MINUTES"] = int(os.environ.get("PASSWORD_RESET_EXPIRY_MINUTES", "30"))
 
